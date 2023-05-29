57612d79ea8db45257ad0e78161ec025717fce33
vblinden <hello@vblinden.dev>
2023-05-29 21:46:28 +0200

    Try out other terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -287,7 +287,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'php' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
@@ -412,6 +412,7 @@ local servers = {
   rust_analyzer = {},
   tsserver = {},
   denols = {},
+  intelephense = {},
 
   lua_ls = {
     Lua = {
