5732e468ab4559bc3e5f8a60ba8f46be6cbe3d70
vblinden <hello@vblinden.dev>
2023-06-30 17:42:20 +0200

    Easy way to switch back to previous buffer

--- nvim/lua/custom/plugins/keymaps.lua
+++ nvim/lua/custom/plugins/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<leader>ktr', ':Neotree reveal position=right<cr>', { desc 
 -- General keymaps
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<leader>b', ':b#<cr>', { desc = 'Switch [B]uffer' })
 
 -- Telescope keymaps
 vim.keymap.set('n', '<leader>si', ':Telescope find_files hidden=true no_ignore=true<cr>', { desc = 'Find [I]nvisible' })
