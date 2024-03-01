d1d5be7ef0a5ececb275b40fa34562ee6446f9a8
vblinden <hello@vblinden.dev>
2024-03-01 09:34:43 +0100

    updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("Berkeley Mono")
+-- config.font = wezterm.font("Berkeley Mono")
 
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.audible_bell = "Disabled"
