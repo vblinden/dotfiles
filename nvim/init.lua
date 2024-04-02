93b6c402fdc1c9a002b3c0907b68da219bb425c1
vblinden <hello@vblinden.dev>
2024-04-02 19:59:09 +0200

    Updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -647,13 +647,22 @@ require("lazy").setup({
 
 	-- Colorscheme
 	{
-		"folke/tokyonight.nvim",
+		"navarasu/onedark.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			require("onedark").setup({
+				code_style = {
+					comments = "none",
+					keywords = "none",
+					functions = "none",
+					strings = "none",
+					variables = "none",
+				},
+			})
 			-- load the colorscheme here
 			vim.o.background = "dark"
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("onedark")
 		end,
 	},
 
