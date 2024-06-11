5a903d461e81079d3a65c5ee08095f35d7e604fa
vblinden <hello@vblinden.dev>
2024-06-11 18:41:51 +0200

    Enable integrations

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,13 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({})
+		require("catppuccin").setup({
+			integrations = {
+				neotree = true,
+				which_key = true,
+				barbar = true,
+			},
+		})
 
 		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
