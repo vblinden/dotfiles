0c1576ed49a0f8b0cd8317f8e5ce69cac554a34b
vblinden <hello@vblinden.dev>
2024-12-13 15:03:41 +0100

    update dependencies

--- nvim/lua/plugins/sync.lua
+++ nvim/lua/plugins/sync.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*",
+	callback = function()
+		local current_file = vim.fn.expand("%:p")
+		local folder = "/Users/vblinden/Code/teamblue/customer-frontend"
+
+		if vim.startswith(current_file, folder) then
+			vim.fn.SyncUploadFile()
+		end
+	end,
+})
+
 return {
 	"eshion/vim-sync",
 	dependencies = {
