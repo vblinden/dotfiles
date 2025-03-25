b16223c94254aecc2a642acff86de473e4540a1e
vblinden <hello@vblinden.dev>
2025-03-25 11:33:33 +0100

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,17 @@
 return {
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			styles = {
-				comments = { italic = false },
-				keywords = { italic = false },
-				functions = { italic = false },
-				variables = { italic = false },
+		require("catppuccin").setup({
+			integrations = {
+				neotree = true,
+				which_key = true,
+				barbar = true,
 			},
-
-			---@param colors ColorScheme
-			on_colors = function(colors) end,
-
-			---@param highlights tokyonight.Highlights
-			---@param colors ColorScheme
-			on_highlights = function(highlights, colors) end,
+			no_italic = true,
 		})
 
-		vim.cmd([[colorscheme tokyonight]])
+		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
 }
-
--- return {
--- 	"catppuccin/nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("catppuccin").setup({
--- 			integrations = {
--- 				neotree = true,
--- 				which_key = true,
--- 				barbar = true,
--- 			},
--- 			no_italic = true,
--- 		})
---
--- 		vim.cmd.colorscheme("catppuccin-frappe")
--- 	end,
--- }
