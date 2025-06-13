4c6906997c7f4694e037bb0472cae2015e5811fe
vblinden <hello@vblinden.dev>
2025-06-13 08:59:44 +0200

    fix colors

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,4 +1,6 @@
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 return {
-  'rmagatti/auto-session',
-  opts = {},
+	"rmagatti/auto-session",
+	opts = {},
 }
