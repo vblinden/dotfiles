16b39373d77295599cda0bb680e0e72e5a845c03
vblinden <hello@vblinden.dev>
2025-06-16 15:37:09 +0200

    update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,28 +1,78 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				auto_trigger = true,
-				keymap = {
-					accept = "<C-k>",
-					next = "<C-l>",
-					prev = "<C-h>",
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					keymap = {
+						accept = "<C-k>",
+						next = "<C-l>",
+						prev = "<C-h>",
+					},
 				},
-			},
-			-- copilot_model = "",
-		})
-	end,
-}
+			})
+		end,
+	},
 
--- return {
--- 	"github/copilot.vim",
--- 	config = function()
--- 		vim.keymap.set("i", "<C-k>", 'copilot#Accept("\\<CR>")', {
--- 			expr = true,
--- 			replace_keycodes = false,
--- 		})
--- 	end,
--- }
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<LocalLeader>a",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+			-- Expand 'cc' into 'CodeCompanion' in the command line
+			vim.cmd([[cab cc CodeCompanion]])
+
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "openai",
+					},
+					inline = {
+						adapter = "openai",
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown", "codecompanion" },
+	},
+
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		opts = {
+			preview = {
+				filetypes = { "markdown", "codecompanion" },
+				ignore_buftypes = {},
+			},
+		},
+	},
+
+	{
+		"echasnovski/mini.diff",
+		config = function()
+			local diff = require("mini.diff")
+			diff.setup({
+				-- Disabled by default
+				source = diff.gen_source.none(),
+			})
+		end,
+	},
+}
