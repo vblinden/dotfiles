54c2c87cbdc8d5a21f3e20eccda75e9a25585512
vblinden <hello@vblinden.dev>
2025-06-19 10:17:55 +0200

    update

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -22,14 +22,6 @@ return {
 		end,
 	},
 
-	-- Highlight todo, notes, etc in comments
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-	},
-
 	-- Collection of various small independent plugins/modules
 	{
 		"echasnovski/mini.nvim",
