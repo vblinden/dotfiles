87c15040738fd0db1a4220a84052b61a45b7f4e5
vblinden <hello@vblinden.dev>
2025-07-08 08:42:41 +0200

    update

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,42 +1,35 @@
--- return {
--- 	"sainnhe/gruvbox-material",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.g.gruvbox_material_enable_italic = false
--- 		vim.g.gruvbox_material_disable_italic_comment = true
--- 		vim.g.gruvbox_material_cursor = "red"
--- 		vim.g.gruvbox_material_background = "soft"
--- 		vim.g.gruvbox_material_better_performance = 1
---
--- 		vim.cmd.colorscheme("gruvbox-material")
--- 	end,
--- }
-
 return {
-	"catppuccin/nvim",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			show_end_of_buffer = true,
-			transparent_background = false,
-			dim_inactive = {
-				enabled = true,
-				shade = "dark",
-				percentage = 0.15,
+		require("tokyonight").setup({
+			style = "storm",
+			light_style = "day",
+			transparent = false,
+			terminal_colors = true,
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false },
+				functions = {},
+				variables = {},
+				sidebars = "dark",
+				floats = "dark",
 			},
-			integrations = {
-				which_key = true,
-				barbar = true,
-				treesitter = true,
-				blink_cmp = true,
-				mason = true,
-				fzf = true,
-			},
-			no_italic = true,
-			no_bold = true,
+			day_brightness = 0.3,
+			dim_inactive = false,
+			lualine_bold = false,
+
+			---@param colors ColorScheme
+			on_colors = function(colors) end,
+
+			---@param highlights tokyonight.Highlights
+			---@param colors ColorScheme
+			on_highlights = function(highlights, colors) end,
+
+			cache = true,
 		})
 
-		vim.cmd.colorscheme("catppuccin-frappe")
+		vim.cmd("colorscheme tokyonight-storm")
 	end,
 }
