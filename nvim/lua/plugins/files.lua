d59cbf4678cd96b87a2ff6722f8008aeaee30011
vblinden <hello@vblinden.dev>
2025-07-21 20:42:13 +0200

    update oil.nvim files

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -1,12 +1,27 @@
-vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open tree" })
+-- vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open tree" })
+vim.keymap.set("n", "-", "<cmd>Fyler<cr>", { desc = "Open tree" })
 
 return {
-	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			watch_for_changes = true,
-		})
-	end,
+	"A7Lavinraj/fyler.nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	opts = {
+		default_explorer = true,
+		views = {
+			explorer = {
+				kind = "split:right",
+				width = 0.3,
+			},
+		},
+	},
 }
+
+-- return {
+-- 	"stevearc/oil.nvim",
+-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+-- 	config = function()
+-- 		require("oil").setup({
+-- 			default_file_explorer = true,
+-- 			watch_for_changes = true,
+-- 		})
+-- 	end,
+-- }
