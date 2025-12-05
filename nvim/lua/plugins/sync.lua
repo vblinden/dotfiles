5fb87b0f59890d8fca3d0b5fce588b9e16db921b
vblinden <hello@vblinden.dev>
2025-12-05 09:18:21 +0100

    add sync

---
+++ nvim/lua/plugins/sync.lua
@@ -0,0 +1,24 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*",
+  callback = function()
+    local current_file = vim.fn.expand("%:p")
+    local home_dir = vim.fn.expand("$HOME")
+    local folder = home_dir .. "/Code/teamblue/customer-frontend"
+
+    if vim.startswith(current_file, folder) then
+      vim.fn.SyncUploadFile()
+    end
+  end,
+})
+
+vim.keymap.set("n", "<leader>dl", "<cmd>:call SyncDownloadFile()<CR>", { desc = "Sync [D]ownload file" })
+
+return {
+  "eshion/vim-sync",
+  dependencies = {
+    "skywind3000/asyncrun.vim",
+  },
+  config = function()
+    vim.keymap.del("n", "<leader>sd")
+  end,
+}
